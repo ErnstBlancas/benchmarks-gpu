92c63b118c8177b32912dff36997468103f2ea24
Ernesto Blancas <blancase78@gmail.com>
2026-04-29 12:25:54 +0100

    small iter0

---
+++ small/run-finetune.py
@@ -0,0 +1,23 @@
+import os, glob, logging, sys, warnings
+warnings.filterwarnings("ignore")
+from mace.cli.run_train import main as mace_run_train_main
+from subprocess import run
+import torch
+################ INPUT #################
+# Check number of cpus/threads availables. If not specified, use all available
+
+try:
+    torch.set_num_threads(int(os.environ['OMP_NUM_THREADS']))
+    print('Running using: ',int(os.environ['OMP_NUM_THREADS']), ' threads')
+except KeyError:
+    torch.set_num_threads(os.cpu_count())
+    print('Running using: ',os.cpu_count(), ' threads')
+
+
+
+def train_mace(config_file_path):
+    logging.getLogger().handlers.clear()
+    sys.argv = ["program", "--config", config_file_path]
+    mace_run_train_main()
+
+train_mace('config.yaml')
